908cf80e980699235063b9c7621eef5056f6ada3
adorechic <adorechic@gmail.com>
2015-12-06 13:06:58 +0900

    Save message on brain

--- lib/ruboty/handlers/good_night.rb
+++ lib/ruboty/handlers/good_night.rb
@@ -1,10 +1,23 @@
 module Ruboty
   module Handlers
     class GoodNight < Base
+      BRAIN_NAMESPACE = 'ruboty-goodnight'
+
       on /おやすみ/, name: 'greet', description: 'greet to ruboty'
 
       def greet(message)
+        save(message)
         message.reply('まだ寝ないよ！')
+      end
+
+      private
+
+      def save(message)
+        room = message.original.except(:robot)
+      end
+
+      def room
+        robot.brain.data[BRAIN_NAMESPACE]
       end
     end
   end
